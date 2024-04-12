f4e749cfe932fcd8ba3deb40de6438f2e0853a79
Harshsahu9898 <harshsahu9898@gmail.com>
2024-04-12 16:25:47 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,30 @@
+// functions
+function sayHello(){
+    console.log("hello harsh")
+}
+// sayHello()
+
+function add(a,b){//a and b are arguments
+    console.log(a+b)
+}
+// add(5,4)//5 and 4 are parameters
+
+function multiply(a,b){
+    // console.log(num1*num2)
+    return a*b//evaluate a and b and return the value and it can be used anywhere
+}
+
+// multiply(23,2); 
+// let x = multiply(23,2);
+// console.log("the multiplication is",x)
+
+// add multiple numbers/ add unlimited numbers
+function addmultiples(){
+    let ans = 0;
+    for(let i =0 ;i<arguments.length;i++){
+        ans += arguments[i];
+    }
+    return ans
+}
+let result = addmultiples(1,2,3,4,5);
+console.log(result);
